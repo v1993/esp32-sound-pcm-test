619ab99ee33c489681fa3bd32c76fa1b17fad4c9
v1993 <v19930312@gmail.com>
2018-08-05 20:21:43 +0300

    Clean code, add comments

--- main/main.cpp
+++ main/main.cpp
@@ -5,37 +5,15 @@
 
 USING_NS_SOUND;
 
-std::shared_ptr<SoundMixer> player;
+std::unique_ptr<SoundMixer> player; // Global to prevent destroying
 
-const extern long unsigned int voice_length; // Nice!
+const extern long unsigned int voice_length;
 const extern uint8_t voice_data[];
 
 
-void play_voice() {
-	{
-		auto provider = std::make_shared<SoundProviderPcm>(voice_data, voice_length, 8000);
-		player = std::make_unique<SoundMixer>(1, 0, DAC_GPIO25_CHANNEL);
-		provider->repeat = false;
-		player->play(0, provider);
-		vTaskDelay(100);
-		std::cout << provider.use_count() << std::endl;
-	}
-	while (true) {vTaskDelay(100);};
-}
-
-
-/*
-void play_voice() {
-	auto provider = std::make_shared<SoundProviderPcm>(voice_data, voice_length, 8000);
-	player = std::make_unique<SoundMixer>(1, 0, DAC_GPIO25_CHANNEL);
-	provider->repeat = true;
-	player->play(0, provider.get());
-	vTaskDelay(100);
-	std::cout << provider.use_count() << std::endl;
-	while (true) {vTaskDelay(100);};
-}
-*/
-
 extern "C" void app_main() {
-	play_voice();
+	player = std::make_unique<SoundMixer>(1, 0, DAC_GPIO25_CHANNEL); // Create SoundMixer
+	auto provider = std::make_shared<SoundProviderPcm>(voice_data, voice_length, 8000); // Create our provider
+	provider->repeat = false; // Change to true for looping
+	player->play(0, provider); // Play on channel 0 (only aviable)
 }
